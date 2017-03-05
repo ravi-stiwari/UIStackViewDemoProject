09df2716c5b6c4a8c97f288409d5ae27552d481e
Ravi Tiwari <rtiwari@digite.com>
2017-03-05 16:38:09 +0530

    Adding blank arranged project

---
+++ UIStackViewDemoProject/MainVC.swift
@@ -0,0 +1,18 @@
+//
+//  MainVC.swift
+//  UIStackViewDemoProject
+//
+//  Created by Ravi Tiwari on 3/5/17.
+//  Copyright © 2017 SelfStudy. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
